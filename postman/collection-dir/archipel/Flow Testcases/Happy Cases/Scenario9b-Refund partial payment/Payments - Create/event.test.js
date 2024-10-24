8df591b58187382b08591ffaf111193d1eee479f
gbague <guillaume.bague@flowbird.group>
2024-10-24 13:33:29 +0200

    PPDEV-13069 - Add Refund tests for archipel connector

--- postman/collection-dir/archipel/Flow Testcases/Happy Cases/Scenario9b-Refund partial payment/Payments - Create/event.test.js
+++ postman/collection-dir/archipel/Flow Testcases/Happy Cases/Scenario9b-Refund partial payment/Payments - Create/event.test.js
@@ -1,20 +1,17 @@
 // Validate status 2xx
-pm.test("[POST]::/payments/:id/confirm - Status code is 2xx", function () {
+pm.test("[POST]::/payments - Status code is 2xx", function () {
   pm.response.to.be.success;
 });
 
 // Validate if response header has matching content-type
-pm.test(
-  "[POST]::/payments/:id/confirm - Content-Type is application/json",
-  function () {
-    pm.expect(pm.response.headers.get("Content-Type")).to.include(
-      "application/json",
-    );
-  },
-);
+pm.test("[POST]::/payments - Content-Type is application/json", function () {
+  pm.expect(pm.response.headers.get("Content-Type")).to.include(
+    "application/json",
+  );
+});
 
 // Validate if response has JSON Body
-pm.test("[POST]::/payments/:id/confirm - Response has JSON Body", function () {
+pm.test("[POST]::/payments - Response has JSON Body", function () {
   pm.response.to.have.jsonBody();
 });
 
@@ -37,7 +34,6 @@ if (jsonData?.payment_id) {
   );
 }
 
-
 // pm.collectionVariables - Set client_secret as variable for jsonData.client_secret
 if (jsonData?.client_secret) {
   pm.collectionVariables.set("client_secret", jsonData.client_secret);
@@ -51,30 +47,24 @@ if (jsonData?.client_secret) {
   );
 }
 
-// Response body must not have value "failed" for "status"
-if (jsonData?.status) {
-  pm.test(
-    "[POST]::/payments/:id/confirm - Content check if value for 'status' not matches 'failed'",
-    function () {
-      pm.expect(jsonData.status).to.not.eql("failed");
-    },
-  );
-}
-
 // Response body should have value "succeeded" for "status"
 if (jsonData?.status) {
   pm.test(
-    "[POST]::/payments/:id/confirm - Content check if value for 'status' matches 'succeeded'",
+    "[POST]::/payments - Content check if value for 'status' matches 'succeeded'",
     function () {
       pm.expect(jsonData.status).to.eql("succeeded");
     },
   );
 }
 
-// pm.collectionVariables - Set mandate_id as variable for jsonData.mandate_id
-if (jsonData?.mandate_id) {
-   pm.collectionVariables.set("mandate_id", jsonData.mandate_id);
-   console.log("- use {{mandate_id}} as collection variable for value",jsonData.mandate_id);
-} else {
-   console.log('INFO - Unable to assign variable {{mandate_id}}, as jsonData.mandate_id is undefined.');
-};
+// Response body should have value "500" for "amount"
+pm.test("[POST]::/payments - Content check if value for 'amount' matches '500'",
+function () {
+    pm.expect(jsonData.amount).to.eql(500);
+});
+
+// Response body should have "EUR" for "currency"
+pm.test("[POST]::/payments - Content check if value for 'currency' matches 'EUR'",
+function () {
+    pm.expect(jsonData.currency).to.eql("EUR");
+});
