4025a17736137e8a27489ffefe1a047aa7271698
Michal Czernecki <michal.czerencki@flowbird.group>
2025-01-23 10:09:49 +0100

    feat: Archipel connector
    Flowbird Archipel connector implementation
    
    Co-authored-by: Guillaume Bague <guillaume.bague@flowbird.group>

--- crates/connector_configs/src/connector.rs
+++ crates/connector_configs/src/connector.rs
@@ -114,6 +114,8 @@ pub struct ConfigMetadata {
     pub source_balance_account: Option<InputData>,
     pub brand_id: Option<InputData>,
     pub destination_account_number: Option<InputData>,
+    pub tenant_id: Option<InputData>,
+    pub platform_url: Option<InputData>,
 }
 
 #[serde_with::skip_serializing_none]
@@ -157,6 +159,7 @@ pub struct ConnectorConfig {
     #[cfg(feature = "payouts")]
     pub adyenplatform_payout: Option<ConnectorTomlConfig>,
     pub airwallex: Option<ConnectorTomlConfig>,
+    pub archipel: Option<ConnectorTomlConfig>,
     pub authorizedotnet: Option<ConnectorTomlConfig>,
     pub bamboraapac: Option<ConnectorTomlConfig>,
     pub bankofamerica: Option<ConnectorTomlConfig>,
@@ -325,6 +328,7 @@ impl ConnectorConfig {
             Connector::Adyen => Ok(connector_data.adyen),
             Connector::Adyenplatform => Err("Use get_payout_connector_config".to_string()),
             Connector::Airwallex => Ok(connector_data.airwallex),
+            Connector::Archipel => Ok(connector_data.archipel),
             Connector::Authorizedotnet => Ok(connector_data.authorizedotnet),
             Connector::Bamboraapac => Ok(connector_data.bamboraapac),
             Connector::Bankofamerica => Ok(connector_data.bankofamerica),
